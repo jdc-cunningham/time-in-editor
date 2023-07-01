2e2c48075b8b113db38e17fe5c91093984df3945
Jacob Cunningham <jcun4128@gmail.com>
2023-06-30 19:31:55 -0500

    have something working

--- tutorial/DEVLOG.md
+++ tutorial/DEVLOG.md
@@ -18,3 +18,67 @@ I will use globalState (didn't really want to)
 
 Need to figure out how to run a function when the editor has focus (and no longer does?)
 
+6:34 PM
+
+grabbed a NOS, cleaned fingers/keyboard damn oily keyboard fingers
+
+6:37 PM
+
+cool so I have a way to display the output as a little toast thing, can get more fancier but I just want hours/mins as a starting point
+
+https://stackoverflow.com/a/58139566/2710227
+
+https://stackoverflow.com/a/58139566/2710227
+
+https://code.visualstudio.com/api/references/when-clause-contexts
+
+6:52 PM
+
+hmm... I broke it, doesn't open the extension host anymore
+
+6:57 PM
+
+I have that message again about markdown hmm
+
+6:59 PM
+
+that did fix it, I just realized this extension won't start unless I type it into the search bar so that's not good, has to start on its own
+
+7:01 PM
+
+okay this looks helpful, does use an activationEvent
+
+https://stackoverflow.com/a/66572953/2710227
+
+oh... it tries to run Markdown if that file is focused, not where it is, interesting
+
+7:04 PM
+
+yeah so if I have extension.js focused and run F5 it'll work, the above does activate extension on load
+
+need to bind the editor focus
+
+7:13 PM
+
+I'm failing
+
+7:19 PM
+
+looking at this list of actions I can bind to
+
+https://gist.github.com/skfarhat/4e88ef386c93b9dceb98121d9457edbf
+
+7:28 PM
+
+I found something I can use, on file cahnge...
+
+problem with that is you can leave an editor open without changing a file... then large gap in time seems like working for a while
+
+7:30 PM
+
+idk I could work with that... typing into a file fires this event
+
+I can make some kind of debouncer, to batch events
+
+Hmm
+
--- tutorial/extension.js
+++ tutorial/extension.js
@@ -9,21 +9,40 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  let timeInEditor = 0;
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
+	console.log('Congratulations, your extension "time-in-editor" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.timeInEditor', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage(`time in editor: ${timeInEditor}`);
 	});
 
 	context.subscriptions.push(disposable);
+
+	// https://stackoverflow.com/a/70340951/2710227
+	// workbench.action.focusActiveEditorGroup
+	// workbench.action.focusPanel
+	// let focusDisposable = vscode.commands.registerCommand('workbench.action.focusActiveEditorGroup', (event) => focusFn(event));
+
+	// const focusFn = (focusDisposable) => {
+	// 	focusDisposable.dispose();
+
+	// 	console.log('focus');
+	// }
+
+	// context.subscriptions.push(focusDisposable);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		console.log('change');
+	});
 }
 
 // this method is called when your extension is deactivated
